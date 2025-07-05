cd3720fb97075f018c41f4980e09901ba6ae640e
joaquinrios-unnoba <jrios912@comunidad.unnoba.edu.ar>
2025-07-05 17:28:01 -0300

    los controladores ahora puede devolver una o todas las intancias de song o playlst, Nota: posiblemente de seban agregar mas metodos y adicionar logica en otros

--- playlist-service/src/main/java/com/prog_distribuida_y_concurrente/spring_microservice/resource/PlaylistResource.java
+++ playlist-service/src/main/java/com/prog_distribuida_y_concurrente/spring_microservice/resource/PlaylistResource.java
@@ -1,5 +1,36 @@
 package com.prog_distribuida_y_concurrente.spring_microservice.resource;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.prog_distribuida_y_concurrente.spring_microservice.model.Playlist;
+import com.prog_distribuida_y_concurrente.spring_microservice.service.PlaylistService;
+
+@Controller
+@RequestMapping("/playlists")
 public class PlaylistResource {
+
+    private PlaylistService playlistService;
+
+    @Autowired
+    public PlaylistResource(PlaylistService playlistService){
+        this.playlistService = playlistService;
+    }
+
+    @GetMapping
+    public List<Playlist> getPlaylists(){
+        return playlistService.getPlaylists();
+    }
+
+    @GetMapping("/(id)")
+    public Playlist gePlaylist(@PathVariable("id") Long id){ //D. Se debe incluir en la respuesta el listado de todas las canciones (id y nombre) que se incluyen en la playlist.
+
+        return playlistService.findById(id);
+    }
 
 }
--- song-service/src/main/java/com/prog_distribuida_y_concurrente/spring_microservice/resource/SongResource.java
+++ song-service/src/main/java/com/prog_distribuida_y_concurrente/spring_microservice/resource/SongResource.java
@@ -1,11 +1,37 @@
 package com.prog_distribuida_y_concurrente.spring_microservice.resource;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.prog_distribuida_y_concurrente.spring_microservice.model.Song;
+import com.prog_distribuida_y_concurrente.spring_microservice.service.SongService;
 
 @Controller
 @RequestMapping("/songs")
 public class SongResource {
+
+    private SongService songService;
+
+    @Autowired
+    public SongResource(SongService songService){
+        this.songService = songService;
+    }
+
+    @GetMapping
+    public List<Song> getSongs(){
+        return songService.getSongs();
+    }
+
+    @GetMapping("/{id}")
+    public Song getSong(@PathVariable("id") Long songId){
+        return songService.findById(songId);
+    }
+
 
     
 }
