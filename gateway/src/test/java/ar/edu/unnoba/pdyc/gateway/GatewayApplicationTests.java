a1add97f7fba85e1e9ae582e3c45160d1dbd5837
joaquinrios-unnoba <jrios912@comunidad.unnoba.edu.ar>
2025-07-05 17:43:02 -0300

    se agrego el geteway y eureka-server, falta revisar si funcionan y como integrarlos

---
+++ gateway/src/test/java/ar/edu/unnoba/pdyc/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.pdyc.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
